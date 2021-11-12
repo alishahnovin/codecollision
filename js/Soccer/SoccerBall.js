ad3cc2504a79c1d051b5a759356d34124ac643aa
Alishah Novin <33673282+alishahnovin@users.noreply.github.com>
2021-11-12 12:00:56 -0600

    Add files via upload

---
+++ js/Soccer/SoccerBall.js
@@ -0,0 +1,4 @@
+class SoccerBall extends GameObject
+{
+	radius = 10;
+}
---
+++ js/Soccer/SoccerTeam.js
@@ -0,0 +1,8 @@
+class SoccerTeam extends Team
+{
+	constructor(params)
+	{
+		super(params);
+		this.net = params.net;
+	}
+}
