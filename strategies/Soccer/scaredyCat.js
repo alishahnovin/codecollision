e80eb40a733f863d903ece0b532d964e6c1cf855
Alishah Novin <33673282+alishahnovin@users.noreply.github.com>
2021-11-22 11:39:44 -0600

    Added relative ball angle and distances to each player

--- strategies/Soccer/scaredyCat.js
+++ strategies/Soccer/scaredyCat.js
@@ -20,8 +20,8 @@ class scaredyCat extends SoccerStrategy
 		id, //Represents the player on the field.  (Values: -1, 0, 1)
 		position, // { x, y }
 		field, // { width, height }
-		teamMates, // [ {angle, distance, x, y} ]
-		otherTeam, // [ {angle, distance, x, y} ]
+		teamMates, // [ {angle, distance, x, y, ball:{angle,distance} } ]
+		otherTeam, // [ {angle, distance, x, y, ball:{angle,distance} } ]
 		ball, // {angle, distance, x, y}
 		ownGoal, //{ topPost{angle, distance, x, y},  bottomPost{angle, distance, x, y} }
 		otherGoal, //{ topPost{angle, distance, x, y},  bottomPost{angle, distance, x, y} }
